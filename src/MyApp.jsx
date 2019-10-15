9329187811c02c7aea32f725a2b350bf1c27160e
elif <elif@sast-mfa.com>
2019-10-15 16:30:19 +0200

    Lab1Ex3

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import AppTitle from "./AppTitle";
 
 const generateArray = (n) => [...Array(n).keys()].map(x => ++x)
+const generateRandomArray = (n) => [...Array.from({length: n}, () => Math.ceil(Math.random() * 25))]
 
 const MyApp = () => (
     <div>
         <h1>Minimal React ozdemire</h1>
-        <p>Bundle size: 181 bytes, <br/> Load time of the bundle:  21 ms,
+        <p>Bundle size: 181 bytes, <br/> Load time of the bundle: 21 ms,
             <br/> Last commit SHA1: bc6178f3dacf5977dbefcce409dd3d3c67debe76 </p>
 
-         <AppTitle/>
+        <AppTitle/>
 
-         <p>{generateArray(20).map(x => ' ' + x)}</p>
+        <p>{generateArray(20).map(x => ' ' + x)}</p>
+        <p>{generateRandomArray(10).map(x => ' ' + x)}</p>
 
     </div>
 )
